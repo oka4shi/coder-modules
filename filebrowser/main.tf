129eb8cc0000e16370e03c954e7a9d56edb9d382
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-11-25 22:35:20 +0900

    remove base_path

--- filebrowser/main.tf
+++ filebrowser/main.tf
@@ -117,7 +117,7 @@ resource "coder_app" "filebrowser" {
 }
 
 locals {
-  server_base_path = var.subdomain ? "" : format("/@%s/%s%s/apps/%s", data.coder_workspace_owner.me.name, data.coder_workspace.me.name, var.agent_name != null ? ".${var.agent_name}" : "", var.slug)
+  server_base_path = ""
   url              = "http://localhost:${var.port}${local.server_base_path}"
   healthcheck_url  = "http://localhost:${var.port}${local.server_base_path}/health"
 }
